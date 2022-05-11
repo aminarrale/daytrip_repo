2a82983c21c6a2ab68e1bb83670f94088002b80d
Karim <aarrale12@gmail.com>
2022-05-11 11:27:23 -0500

    Updated day trip generator project

--- daytripgenerator.py
+++ daytripgenerator.py
@@ -29,50 +29,17 @@ def select_item (someArray, type):
 
 chosen_dest = select_item(destinations_list, "destination")
 chosen_rest = select_item(restaurants_list, "restaurant")
+chosen_transportation = select_item(transportation_list, "transportation")
+chosen_entertainment = select_item(entertainment_list, "entertainment")
+
+print("You will arrive in " + chosen_dest + " " + "where you will eat at" + " " + chosen_rest + " " + "and you will take a" + chosen_transportation  + "  and you will be " + chosen_entertainment)
+
+print("Do you confirm your trip is complete?")
+users_response = input("Enter yes: ")
+if users_response == "yes":
+    print("Have a wonderful vacation!")
 
 
-
-
-
-
-random_item_from_list = random.choice(destinations_list)
-print( f"We have selected {random_item_from_list} as your destination, does this sound good? ")
-users_answer = input("Enter yes or no: ")
-if users_answer == "yes":
-    print("Great choice lets move on")
-else:
-    print("How about this destination?")
-   
-
-
-random_item_from_list = random.choice(restaurants_list)
-print( f"We have selected {random_item_from_list} as your restaurant, does this sound good? ")
-users_answer = input("Enter yes or no: ")
-if users_answer == "yes":
-    print("Great choice lets move on")
-else:
-    print("How about this restaurant?")
-
-random_item_from_list = random.choice(transportation_list)
-print( f"We have selected {random_item_from_list} as your form of transportation, does this sound good? ")
-users_answer = input("Enter yes or no: ")
-if users_answer == "yes":
-    print("Great choice lets move on")
-else:
-    print("How about this form of transportation?")
-
-random_item_from_list = random.choice(entertainment_list)
-print( f"We have selected {random_item_from_list} as your entertainment, does this sound good? ")
-users_answer = input("Enter yes or no: ")
-if users_answer == "yes":
-    print("Great choice lets move on")
-else:
-    print("How about this entertainment?")
-
-print("Do you confirm this trip?")
-users_answer = input("")
-if users_answer == "yes":
-    print("Have a wonderful vacation!") 
 
 
 
